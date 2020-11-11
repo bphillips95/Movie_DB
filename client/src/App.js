edf3a8a7bf0b8b6cae0d61788b1cf117eb9efdee
Baruch Phillips <baruchphillips@gmail.com>
2020-11-11 16:49:11 -0500

    added dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [state, setstate] = useState([])
+
+  const URL = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&page=1&r=json"
+
+  useEffect(() => {
+    fetch(URL, {
+      "access-control-allow-credentials": "true",
+      "access-control-allow-origin": "*",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": process.env.REACT_API_KEY,
+        "x-rapidapi-host": process.env.REACT_API_HOST,
+        "Content-Type": 'application/json'
+      }
+    })
+    .then(r => r.json()).then(data => {
+      setstate(data)
+     })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Front-end here
     </div>
   );
 }
