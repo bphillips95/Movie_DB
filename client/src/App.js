7c4af8f168f6a3b61c0bdd8b990277cd60327f0c
Baruch Phillips <baruchphillips@gmail.com>
2020-11-15 17:31:05 -0500

    added search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
 import MovieList from './components/MovieList'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
@@ -8,29 +9,31 @@ function App() {
 
   const URL = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%&page=1&r=json"
 
-  useEffect(() => {
-    fetch(URL, {
-      "headers": {
-        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-        "x-rapidapi-host": process.env.REACT_APP_API_HOST,
-        "Content-Type": 'application/json'
-      }
-    })
-    .then(r => r.json()).then(data => {
-      setstate(data)
-     })
-  }, [])
+  // useEffect(() => {
+  //   fetch(URL, {
+  //     "headers": {
+  //       "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+  //       "x-rapidapi-host": process.env.REACT_APP_API_HOST,
+  //       "Content-Type": 'application/json'
+  //     }
+  //   })
+  //   .then(r => r.json()).then(data => {
+  //     setstate(data)
+  //    })
+  // }, [])
   
   const movies = state?.Search?.map(movie => <MovieList movie={movie}/> )
+
+  const search = info => console.log(info)
 
   // Have to hold movie id, if click on movie fetch movie info with movie id. 
 
   return (
     <div className="App">
       <h3>Front-end here</h3>
-      <input/> Search Bar
+      <SearchBar search={search}/>
       <div>
-       {movies}
+       {/* {movies} */}
       </div>
     </div>
   );
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+function SearchBar({search}) {
+
+    const [state, setstate] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        search(state)
+    }
+
+    return (
+        <div>
+             <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={state.value} onChange={(e) => setstate(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+        </div>
+    )
+}
+
+export default SearchBar
