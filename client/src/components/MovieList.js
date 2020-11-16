f60605be8cd9341e3bb2942320fc48a2fd91a25c
Baruch Phillips <baruchphillips@gmail.com>
2020-11-16 15:19:03 -0500

    added click for info

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function MovieList({movie}) {
 
-    const handleClick = () => {
+    const [state, setstate] = useState([])
 
+    const handleClick = (id) => {
+        fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${id}&r=json`, {
+            "headers": {
+              "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+              "x-rapidapi-host": process.env.REACT_APP_API_HOST,
+              "Content-Type": 'application/json'
+            }
+          })
+          .then(r => r.json()).then(data => {
+                setstate(data)
+               })
     }
-    
+
     return (
         <div>
            <h4>{movie.Title} </h4> 
             <img src={movie.Poster} alt='' onClick={() => handleClick(movie.imdbID)}/>
+            {/* movie info here on click */}
         </div>
     )
 }
